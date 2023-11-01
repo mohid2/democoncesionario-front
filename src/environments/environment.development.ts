1933d06e6d2a9b4efa17f7038e37b3e5db402f87
mohid2 <fukahamaroc@gmail.com>
2023-11-01 14:27:44 +0100

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://democoncesionario-production.up.railway.app/'
+    apiUrl: 'https://democoncesionario-production.up.railway.app'
   };
